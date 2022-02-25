5d3e38ca8b3ba80ffc97b40512eeaeddbda36e12
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-25 15:38:57 +0530

    [Raj] Added:- The sql query to insert data into table

--- AddressBookSql.sql
+++ AddressBookSql.sql
@@ -15,3 +15,12 @@ Create Table AddressBook(
 	PhoneNum bigint,
 	EmailId varchar(50)
 );
+
+---------------------------------------Inserting Value Into The AddressBook Table (UC3)-----------------------------------------------
+Insert Into AddressBook Values('Raj', 'Verma', 'Ghansoli', 'NaviMumbai', 'Maharashtra', 456123, 9517534561, 'abc123@gmail.com'),
+						('Mansi', 'Verma', 'Vashi', 'NaviMumbai', 'Maharashtra', 789456, 9874561230, 'abc456@gmail.com'),	
+						('Ajay', 'Matkar', 'Chembur', 'Mumbai', 'Maharashtra', 965874, 7412589631, 'abc789@gmail.com'),	
+						('Omkar', 'Bhatt', 'Andheri', 'Mumbai', 'Maharashtra', 412563, 9852364170, 'abc852@gmail.com'),	
+						('Aman', 'Nikam', 'Borivali', 'Mumbai', 'Maharashtra', 456123, 9852741630, 'abc741@gmail.com');
+Select * From AddressBook;
+							
